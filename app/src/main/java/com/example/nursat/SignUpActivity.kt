8f3caa5d783ef28ce14a61162e0dc0fc5ddd8098
Nurikcmd <mr.nursat04@gmail.com>
2025-04-03 06:50:40 +0500

    Реализована логика регистрации с проверкой пароля и сохранением в SharedPreferences"

--- app/src/main/java/com/example/nursat/SignUpActivity.kt
+++ app/src/main/java/com/example/nursat/SignUpActivity.kt
@@ -1,4 +1,41 @@
 package com.example.nursat
 
-class SignUpActivity {
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var edUsername: EditText
+    private lateinit var edPassword: EditText
+    private lateinit var edConfirmPassword: EditText
+    private lateinit var btnCreateUser: Button
+    private val CREDENTIAL_SHARED_PREF = "our_shared_pref"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        edUsername = findViewById(R.id.ed_username)
+        edPassword = findViewById(R.id.ed_password)
+        edConfirmPassword = findViewById(R.id.ed_confirm_pwd)
+        btnCreateUser = findViewById(R.id.btn_create_user)
+
+        btnCreateUser.setOnClickListener {
+            val strPassword = edPassword.text.toString()
+            val strConfirmPassword = edConfirmPassword.text.toString()
+            val strUsername = edUsername.text.toString()
+
+            if (strPassword.isNotEmpty() && strConfirmPassword.isNotEmpty() && strPassword == strConfirmPassword) {
+                val credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE)
+                val editor = credentials.edit()
+                editor.putString("Password", strPassword)
+                editor.putString("Username", strUsername)
+                editor.commit()
+
+                finish()
+            }
+        }
+    }
 }
